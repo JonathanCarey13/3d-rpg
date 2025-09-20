90987883ecf6e2dd3cdfca5b0eb42012868f5a67
Jonathan Carey <jonathan.carey1995@gmail.com>
2025-09-20 14:56:23 -0400

    added animations, blended animations

--- Player/player.gd
+++ Player/player.gd
@@ -13,8 +13,9 @@ var _look: Vector2 = Vector2.ZERO
 @export var mouse_sensitivity: float = 0.00075
 @export var min_boundary: float = -60
 @export var max_boundary: float = 10
-@export var character_turn_decay: float = 20.0
+@export var animation_decay: float = 20.0
 
+@onready var rig: Node3D = $RigPivot/Rig
 @onready var horizontal_pivot: Node3D = $HorizontalPivot
 @onready var vertical_pivot: Node3D = $HorizontalPivot/VerticalPivot
 @onready var rig_pivot: Node3D = $RigPivot
@@ -25,6 +26,7 @@ func _ready() -> void:
 
 func _physics_process(delta: float) -> void:
 	frame_camera_rotation()
+	
 	
 	# Add the gravity.
 	if not is_on_floor():
@@ -37,6 +39,8 @@ func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction: Vector3 = get_movement_direction()
+	rig.update_animation_tree(direction)
+	
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
@@ -53,8 +57,10 @@ func _unhandled_input(event: InputEvent) -> void:
 	if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
 		if event is InputEventMouseMotion:
 			_look += -event.relative * mouse_sensitivity
-			
-			
+	if rig.is_idle():
+		if event.is_action_pressed("click"):
+			slash_attack()
+
 func get_movement_direction() -> Vector3:
 	var input_dir: Vector2 = Input.get_vector("move_left", "move_right", "move_forward", "move_back")
 	var input_vector: Vector3 = Vector3(input_dir.x, 0, input_dir.y).normalized()
@@ -79,5 +85,8 @@ func look_toward_direction(direction: Vector3, delta: float) -> void:
 	)
 	rig_pivot.global_transform = rig_pivot.global_transform.interpolate_with(
 		target_transform,
-		character_turn_decay - exp(-character_turn_decay * delta)
+		1.0 - exp(-animation_decay * delta)
 	)
+
+func slash_attack() -> void:
+	rig.travel("Slash")
